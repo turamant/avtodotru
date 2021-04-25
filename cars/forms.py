77375eff031d78656c376d860ab55fcc342ef781
askvart <tur1amant@gmail.com>
2021-04-25 13:35:47 +0300

    comments

--- cars/forms.py
+++ cars/forms.py
@@ -1,9 +1,8 @@
-from django.forms import ModelForm
-
+from django import forms
 from .models import Comment
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('name','email', 'body')
+        fields = ('name' ,'email', 'body')
